975a727930b80c2832df38d7c811b623d1b86562
Gabor Thury <v-gthury@expedia.com>
2023-12-19 15:37:34 +0100

    add OrderClient

--- src/main/java/com/shopping/client/OrderClient.java
+++ src/main/java/com/shopping/client/OrderClient.java
@@ -1,2 +1,31 @@
-package com.shopping.client;public class OrderClient {
+package com.shopping.client;
+
+import com.shopping.stubs.order.Order;
+import com.shopping.stubs.order.OrderRequest;
+import com.shopping.stubs.order.OrderResponse;
+import com.shopping.stubs.order.OrderServiceGrpc;
+import io.grpc.Channel;
+
+import java.util.List;
+
+public class OrderClient {
+    private final OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
+
+    public OrderClient(Channel channel) {
+        this.orderServiceBlockingStub = OrderServiceGrpc.newBlockingStub(channel);
+    }
+
+    public List<Order> getOrders(int userId) {
+        OrderRequest orderRequest = buildRequest(userId);
+        OrderResponse orderResponse = orderServiceBlockingStub.getOrdersForUser(orderRequest);
+
+        return orderResponse.getOrderList();
+    }
+
+    private OrderRequest buildRequest(int userId) {
+        return OrderRequest
+                .newBuilder()
+                .setUserId(userId)
+                .build();
+    }
 }
